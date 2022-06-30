9f34b1ac620109f2612f08735553f64130256daf
boburjonurazbayev <bobur7850@gmail.com>
2022-06-30 14:49:31 +0500

    Add start stop clear

--- js/main.js
+++ js/main.js
@@ -7,11 +7,15 @@ let hour = 0;
 
 const milSecondInSecond = 1000;
 const secondInMinute = 60;
-const minuteInHour = secondInMinute;
+const minuteInHour = 60;
 
 function renderTime(realtime, node) {
   node.textContent = realtime;
 }
+
+let realtime = "0" + hour + " : " + "0" + minute + " : " + "0" + second;
+
+renderTime(realtime, elTime);
 
 function time() {
   let runSecond = setInterval(() => {
@@ -34,15 +38,42 @@ function time() {
     hour++;
   }, minuteInHour * secondInMinute * milSecondInSecond);
 
-  setInterval(() => {
+  let fullTime = setInterval(() => {
     second = String(second).padStart(2, 0);
     minute = String(minute).padStart(2, 0);
     hour = String(hour).padStart(2, 0);
 
-    let realtime = hour + " : " + minute + " : " + second;
+    realtime = hour + " : " + minute + " : " + second;
 
     renderTime(realtime, elTime);
   }, milSecondInSecond);
+
+  btns.addEventListener("click", (evt) => {
+    if (evt.target.id == "stop") {
+      clearInterval(runSecond);
+      clearInterval(runMinute);
+      clearInterval(runHour);
+      clearInterval(fullTime);
+      start.disabled = false;
+      stop.disabled = true;
+    }
+
+    if (evt.target.id == "clear") {
+      hour = 0;
+      minute = 0;
+      second = 0;
+      realtime = "0" + hour + " : " + "0" + minute + " : " + "0" + second;
+
+      renderTime(realtime, elTime);
+
+      start.disabled = false;
+      stop.disabled = true;
+    }
+  });
 }
 
-time();
+start.addEventListener("click", (evt) => {
+  time();
+  start.disabled = true;
+  stop.disabled = false;
+});
